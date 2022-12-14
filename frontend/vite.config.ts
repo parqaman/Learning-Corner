e3c3ecbcd203d2323e0a5312eace90b0e4ee79b8
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2022-12-14 12:40:30 +0100

    updated frontend port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5432,
+    port: 4000,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
