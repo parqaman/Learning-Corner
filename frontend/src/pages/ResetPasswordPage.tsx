497f40f1c4a200f5bb7a92e400a52be24ebb4628
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2022-12-22 14:11:36 +0100

    reset password handling

--- frontend/src/pages/ResetPasswordPage.tsx
+++ frontend/src/pages/ResetPasswordPage.tsx
@@ -1,26 +1,16 @@
-import { object, string } from 'yup'
 import { AuthCard } from '../components/AuthCard'
 import { AppLayout } from '../layout/AppLayout'
-import { Heading, Box, VStack, FormControl, Button, FormErrorMessage, Input } from '@chakra-ui/react'
+import { useToast, Heading, Box, Text, Button, Input } from '@chakra-ui/react'
 import { useApiClient } from '../adapter/api/useApiClient';
 import { useAuth } from '../providers/AuthProvider';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export const ResetPasswordSchema = object({
-  currentPassword: string().required(),
-  newPassword: string().required().min(8)
-});
-
-export type ResetPasswordData = {
-  currentPassword: string;
-  newPassword: string;
-};
-
 export const ResetPasswordPage = () => {
   const apiClient = useApiClient();
   const user = useAuth().user;
   const navigate = useNavigate()
+  const toast = useToast();
 
   const [currentPassword, setCurrentPassword] = useState("")
   const [newPassword, setNewPassword] = useState("")
@@ -33,20 +23,31 @@ export const ResetPasswordPage = () => {
         currentPassword: currentPassword,
         newPassword: newPassword
       })
-      navigate(-1);
+      .then(()=>{
+        navigate(-1);
+      })
+      .catch(error=>{             
+        toast({
+          title: "Error occured.",
+          description: <Text>{error.response.data.errors}</Text>,
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      })
     }
   }
 
   return (
-    <AppLayout>
+    <AppLayout display={'flex'} flexDir='column' justifyContent={'center'} alignItems='center'>
         <AuthCard>
             <Heading >
                 Reset password
             </Heading>
             <form onSubmit={(e) => handleNewPassword(e)}>
               <Box width={'100%'} gap={'1em'} display='flex' flexDirection={'column'}>
-                <Input value={currentPassword} onChange={(e)=>setCurrentPassword(e.target.value)} as={Input} isRequired padding={'0.75em'} variant={'unstyled'} placeholder="Current password" _placeholder={{color: '#8E8E8E'}} border={'solid 2px'} borderColor={'rgba(0, 0, 0, 30%)'} id="email" name="email" type="text" />
-                <Input value={newPassword} onChange={(e)=>setNewPassword(e.target.value)} as={Input} isRequired padding={'0.75em'} variant={'unstyled'} placeholder="New password" _placeholder={{color: '#8E8E8E'}} border={'solid 2px'} borderColor={'rgba(0, 0, 0, 30%)'} id="password" name="password" type="password"/>
+                <Input value={currentPassword} onChange={(e)=>setCurrentPassword(e.target.value)} as={Input} isRequired padding={'0.75em'} variant={'unstyled'} placeholder="Current password" _placeholder={{color: '#8E8E8E'}} border={'solid 2px'} borderColor={'rgba(0, 0, 0, 30%)'} id="current-pass" name="current-pass" type="password" />
+                <Input value={newPassword} onChange={(e)=>setNewPassword(e.target.value)} as={Input} isRequired padding={'0.75em'} variant={'unstyled'} placeholder="New password" _placeholder={{color: '#8E8E8E'}} border={'solid 2px'} borderColor={'rgba(0, 0, 0, 30%)'} id="new-pass" name="new-pass" type="password"/>
                 <Button variant={'unstyled'} width={'100%'} type="submit" bg={'black'} color={'white'} borderRadius={'2rem'}>
                   Reset password
                 </Button>
