a3d32909ced6c5c8a3f570eb4b766cf8682be406
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2022-12-29 21:07:19 +0100

    implemented my courses page

--- frontend/src/components/course_components/CourseList.tsx
+++ frontend/src/components/course_components/CourseList.tsx
@@ -4,7 +4,7 @@ import { SingleCourse } from "./SingleCourse";
 
 export const CourseList = ({courses}: {courses: Course[]}) => {
     return (
-            <Flex flexDir={'column'} alignItems={'center'} mt='0.25rem' mb={'1.5rem'}>
+            <Flex minW={'47rem'} flexDir={'column'} alignItems={'center'} mt='0.25rem' mb={'1.5rem'}>
                 <Grid templateColumns={'repeat(3, 1fr)'} gap='1rem'>
                     {
                         courses ? (
--- frontend/src/pages/MyCoursesPage.tsx
+++ frontend/src/pages/MyCoursesPage.tsx
@@ -1,66 +1,57 @@
-import { SlideFade, Heading, Flex, Box, Text } from '@chakra-ui/react'
+import { SlideFade, Heading, Flex, Box, Text, Button } from '@chakra-ui/react'
 import { CourseList } from '../components/course_components/CourseList'
 import { SingleCourse } from '../components/course_components/SingleCourse'
 import { AppLayout } from '../layout/AppLayout'
 import { useAuth } from '../providers/AuthProvider'
 import { AiOutlinePlus } from 'react-icons/ai'
-import { Course, SearchBar } from './HomePage'
+import { Course, User } from '../adapter/api/__generated/api'
+import { useEffect, useState } from 'react'
+import { useApiClient } from '../adapter/api/useApiClient'
+import { useNavigate } from 'react-router-dom'
 
 
 const NewCourseTile = () => {
+  const navigate = useNavigate()
     return (
-        <Text fontSize={'large'} display='flex' flexDir={'column'} alignItems='center' justifyContent={'flex-end'} mb='0.5rem'>
-            Create a new course?
-        </Text>
+      <Box display='flex' flexDir={'column'} alignItems='center' justifyContent={'flex-end'}>
+        <Button onClick={()=>navigate('/courses/newcourse')} variant={'link'} color='rgba(0, 0, 0, 0.75)' _active={{}} size={'sm'}>
+            Create your own course?
+        </Button>
+      </Box>
     )
 }
 
 export const MyCoursesPage = () => {
-    const user = useAuth().user;
-    const mockCourses: Course[] = [
-        {
-          id: 1,
-          courseName: "Advanced Web Development",
-          courseAuthor: "Author 1"
-        },
-        {
-          id: 2,
-          courseName: "Graphische Datenverarbeitung",
-          courseAuthor: "Author 2"
-        },
-        {
-          id: 3,
-          courseName: "Datenbanken 2",
-          courseAuthor: "Author 3"
-        },
-        {
-          id: 4,
-          courseName: "Data Warehouse Techonologien",
-          courseAuthor: "Author 4"
-        },
-        {
-          id: 5,
-          courseName: "Unix for Developers",
-          courseAuthor: "Author 5"
-        },
-        {
-          id: 6,
-          courseName: "Programmieren Algorithmen und Datenstruktur",
-          courseAuthor: "This is the author 6"
-        },
-      ]
+  const [user, setUser] = useState<User | null>(useAuth().user)
+  const apiClient = useApiClient();
+
+  const fetchUser = async () => {
+    if(user){
+      const fetchedUser = await apiClient.getUsersId(user.id)
+      .then((data) => {
+        setUser(data.data)
+      })
+      .catch((e)=>{
+        console.log(e);
+      })
+    }
+  }
+
+  useEffect(()=>{
+    fetchUser()
+  }, [])
 
   return (
     <AppLayout display={'flex'} flexDir='column' alignItems='center' mt={'3rem'}>
       <SlideFade delay={0.2} in={true}>
         <Box display='flex' flexDirection={'column'}>
-            <Flex mb={'1.5rem'}>
-                <Heading flex={1} fontSize={'5xl'} fontWeight='semibold'>
+            <Flex mb={'1.5rem'} justifyContent={'space-between'}>
+                <Heading fontSize={'5xl'} fontWeight='semibold'>
                     My Courses
                 </Heading>
                 <NewCourseTile/>
             </Flex>
-            <CourseList courses={mockCourses}/>
+            <CourseList courses={user?.courses}/>
         </Box>
       </SlideFade>
     </AppLayout>
