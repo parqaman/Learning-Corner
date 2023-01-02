f7ba49c17588d5a6637b4e18d5abbda867f6deba
Anang Achjar Surya <anang.a.surya@stud.h-da.de>
2023-01-02 09:59:13 +0100

    deleted test route

--- backend/src/controller/course.controller.ts
+++ backend/src/controller/course.controller.ts
@@ -1,60 +1,64 @@
 import { Router } from "express";
 import { DI } from "../";
-import {Course, CreateCourseDTO, CreateCourseSchema} from "../entities";
-import {wrap} from "@mikro-orm/core";
+import { Course, CreateCourseDTO, CreateCourseSchema } from "../entities";
+import { wrap } from "@mikro-orm/core";
 
 const router = Router({ mergeParams: true });
 
 router.get("/", async (req, res) => {
-    return res.json(await DI.courseRepository.findAll({
-        populate: ['sections', 'participants']
-    }));
+  return res.json(
+    await DI.courseRepository.findAll({
+      populate: ["sections", "participants"],
+    })
+  );
 });
 
 router.get("/:id", async (req, res) => {
-    const course = await DI.courseRepository.findOne( {id: req.params.id}, {
-        populate: ['sections', 'participants']
-    });
-    if(!course) return res.status(404).send({ message: 'Course not found' });
-    return res.json(course);
+  const course = await DI.courseRepository.findOne(
+    { id: req.params.id },
+    {
+      populate: ["sections", "participants"],
+    }
+  );
+  if (!course) return res.status(404).send({ message: "Course not found" });
+  return res.json(course);
 });
 
 router.post("/", async (req, res) => {
-  const newCourse = new Course(req.body);
-  await DI.courseRepository.persistAndFlush(newCourse);
+  const validatedData = await CreateCourseSchema.validate(req.body).catch(
+    (e) => {
+      res.status(400).json({ errors: e.errors });
+    }
+  );
+  if (!validatedData) {
+    return;
+  }
+  if (!req.user) {
+    return;
+  }
+  const createCourseDTO: CreateCourseDTO = {
+    ...validatedData,
+    lecturer: req.user,
+  };
 
-  return res.status(201).send(newCourse);
+  const course = new Course(createCourseDTO);
+  const hasSections =
+    createCourseDTO.sections && createCourseDTO.sections.length > 0;
+  const hasParticipants =
+    createCourseDTO.participants && createCourseDTO.participants.length > 0;
+
+  if (hasSections) wrap(course).assign({ sections: createCourseDTO.sections });
+  if (hasParticipants)
+    wrap(course).assign({ participants: createCourseDTO.participants });
+
+  await DI.courseRepository.persistAndFlush(course);
+
+  if (hasSections) await DI.courseRepository.populate(course, ["sections"]);
+  if (hasParticipants)
+    await DI.courseRepository.populate(course, ["participants"]);
+
+  return res.status(201).send(course);
 });
-/*
-router.post("/", async (req, res) => {
-    const validatedData = await CreateCourseSchema.validate(req.body).catch((e) => {
-        res.status(400).json({ errors: e.errors });
-    });
-    if (!validatedData) {
-        return;
-    }
-    if (!req.user) {
-        return;
-    }
-    const createCourseDTO: CreateCourseDTO = {
-        ...validatedData,
-        lecturer: req.user
-    };
-
-    const course = new Course(createCourseDTO);
-    const hasSections = createCourseDTO.sections && createCourseDTO.sections.length > 0;
-    const hasParticipants = createCourseDTO.participants && createCourseDTO.participants.length > 0;
-
-    if(hasSections) wrap(course).assign({sections: createCourseDTO.sections});
-    if(hasParticipants) wrap(course).assign({participants: createCourseDTO.participants});
-
-    await DI.courseRepository.persistAndFlush(course);
-
-    if(hasSections) await DI.courseRepository.populate(course, ['sections']);
-    if(hasParticipants) await DI.courseRepository.populate(course, ['participants']);
-
-    return res.status(201).send(course);
-});*/
 
 // Update a course
 router.put("/:courseId/user/:userId", async (req, res) => {
