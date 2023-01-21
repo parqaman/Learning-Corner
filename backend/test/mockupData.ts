dc05ee3f1140d42097ee9c06e8c4f159aa6da909
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2023-01-21 16:41:43 +0100

    implemented backend course tests

---
+++ backend/test/mockupData.ts
@@ -0,0 +1,36 @@
+import { Course, Message, Section, User } from "../src/entities";
+
+export const mockup_user = new User({
+    firstName: "John",
+    lastName: "Doe",
+    email: "johndoe@gmail.com",
+    password: "12345678",
+    photo: "profile_empty.png"
+});
+
+export const mockup_user_2 = new User({
+    firstName: "Alex",
+    lastName: "Turner",
+    email: "alexturner@gmail.com",
+    password: "12345678",
+    photo: "profile_empty.png"
+});
+
+export const mockup_course = new Course({
+    name: "My Course",
+    description: "A description",
+    lecturer: mockup_user,
+});
+
+export const mockup_section = new Section({
+    heading: "Mockup Section",
+    description: "this is a mockup section",
+    text: "section test"
+});
+
+export const mockup_message = new Message({
+    sender: mockup_user,
+    message: "Hello World",
+    time: Date.now.toString(),
+    roomId: ""
+})
