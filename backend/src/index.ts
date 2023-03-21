1a51f126eb4e9682c2524e8b02a1520bff99cf7f
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2023-03-21 10:40:23 +0100

    updated cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -93,7 +93,10 @@ export const initializeServer = async () => {
   app.use(express.json({ limit: '5mb' }));
   app.use((req, res, next) => RequestContext.create(DI.orm.em, next));
   app.use(Auth.prepareAuthentication);
-  app.use(cors())
+  app.use(cors({
+    origin: '*',
+    credentials: true,
+  }))
 
   // routes
   app.use(express.static(clientPath));
